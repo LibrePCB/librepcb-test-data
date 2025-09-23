7f0129aa7a6336cda2691cb26fb6dc405080831d
U. Bruhin <urbibruhin@bluewin.ch>
2025-09-23 13:05:10 +0200

    Add i18n test files

---
+++ i18n/lupdate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+lupdate -no-obsolete -locations none -tr-function-alias "tr=notr" ../../unittests -ts unittests_de.ts
---
+++ i18n/unittests_de.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" sourcelanguage="en">
+<context>
+    <name>ui::GuiApplicationTest</name>
+    <message>
+        <source>Untranslated String</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Translated String</source>
+        <translation>Übersetzter Text</translation>
+    </message>
+    <message>
+        <source>Translated String %1 of %2</source>
+        <translation>Übersetzter Text %1 von %2</translation>
+    </message>
+    <message numerus="yes">
+        <source>Translated %n String(s)</source>
+        <translation>
+            <numerusform>Übersetzter %n Text</numerusform>
+            <numerusform>Übersetzte %n Texte</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>Translated String ‒ With Unicode ☺</source>
+        <translation>Übersetzter Text ‒ Mit Unicode ☺</translation>
+    </message>
+</context>
+</TS>
